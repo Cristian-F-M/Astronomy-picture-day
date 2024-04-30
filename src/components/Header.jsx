1cedaed7350b2dadc6dcb73a12caa83d856c4ef7
Cristian Morales <cfmorales.diaz@gmail.com>
2024-04-30 11:16:21 -0500

    Add Header component with date picker functionality

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import { useId } from "react";
+
+export function Header({ date, handleChangeDate }) {
+    const dateId = useId();
+
+    const handleClickDate = ({ target }) => {
+        target.showPicker();
+    };
+
+    return (
+        <>
+            <header className="mt-2 font-semibold flex items-center flex-col">
+                <h1 className='text-4xl' >Astronomy Picture of the Day</h1>
+                <form action="" className="my-1 w-fit text-xl">
+                    <label htmlFor={dateId}>
+                        <input
+                            onClick={handleClickDate}
+                            onChange={handleChangeDate}
+                            id={dateId}
+                            type="date"
+                            value={date}
+                            className="bg-transparent"
+                        />
+                    </label>
+                </form>
+            </header>
+        </>
+    );
+}
