bd9dee6e90f01682c88b61e1afa4361f24084290
Cristian-F-M <cfmorales.diaz@gmail.com>
2024-05-13 11:29:17 -0500

    Update tailwind.config.mjs to add responsive breakpoints

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
 		extend: {},
+		screens: {
+			'm-2xl': {'max': '1535px'},
+			'm-xl': {'max': '1279px'},
+			'm-lg': {'max': '1023px'},
+			'm-md': {'max': '767px'},
+			'm-sm': {'max': '639px'},
+			...defaultTheme.screens,
+		}
 	},
 	plugins: [],
 }
